8b0ad99591cf77ea391501daf0ff93ad137d441b
Hans Sy <55111820+hanssy130@users.noreply.github.com>
2021-03-13 13:47:21 -0800

    added dice functions and started readme

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const client = new Discord.Client();
 
 const prefix = '!';
 
-client.on('message', function(message) {
+client.on('message', function (message) {
 	if (message.author.bot) return;
 	if (!message.content.startsWith(prefix)) return;
 
@@ -16,6 +16,25 @@ client.on('message', function(message) {
 		const timeTaken = Date.now() - message.createdTimestamp;
 		message.reply(`Pong! This message had a latency of ${timeTaken}ms.`);
 	}
+
+	if (command === 'diceroll') {
+		console.log(args);
+
+		if (args[0] && args[1]) {
+			for (let i = 0; i < args[0]; i++) {
+				message.channel.send(rollDice(args[1]));
+			}
+		} else if (args[0]) {
+			message.channel.send(rollDice(args[0]));
+		} else {
+			message.channel.send(rollDice(6));
+		}
+	}
 });
+
+function rollDice(x) {
+	console.log("Rolling: " + x);
+	return Math.ceil(Math.random() * x);
+}
 
 client.login(config.BOT_TOKEN);
