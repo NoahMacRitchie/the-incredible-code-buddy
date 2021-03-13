8b29fe7820a5a35a93d66fa8d581601fee6d3653
agoryelov <andrey.goryelov@gmail.com>
2021-03-13 11:23:43 -0800

    Added foldering

--- index.js
+++ index.js
@@ -1,21 +1,43 @@
+const fs = require('fs');
 const Discord = require('discord.js');
 const config = require('./config.json');
+
 const client = new Discord.Client();
+client.commands = new Discord.Collection();
 
 const prefix = '!';
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
-client.on('message', function(message) {
-	if (message.author.bot) return;
-	if (!message.content.startsWith(prefix)) return;
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
+}
 
-	const commandBody = message.content.slice(prefix.length);
-	const args = commandBody.split(' ');
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.on('message', message => {
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).trim().split(/ +/);
 	const command = args.shift().toLowerCase();
 
-	if (command === 'ping') {
+    if (command === 'ping') {
 		const timeTaken = Date.now() - message.createdTimestamp;
 		message.reply(`Pong! This message had a latency of ${timeTaken}ms.`);
+        return;
+	}
+
+	if (!client.commands.has(command)) return;
+
+	try {
+		client.commands.get(command).execute(message, args);
+	} catch (error) {
+		console.error(error);
+		message.reply('there was an error trying to execute that command!');
 	}
 });
+
 
 client.login(config.BOT_TOKEN);
