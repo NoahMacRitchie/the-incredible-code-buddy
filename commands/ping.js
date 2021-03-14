3027e9a1bc12668ae4cc4b961f33da96c57a1f80
Young <youngkwon313@gmail.com>
2021-03-13 16:15:52 -0800

    fix merge conflicts

--- commands/ping.js
+++ commands/ping.js
@@ -1,8 +1,8 @@
 module.exports = {
-	name: 'ping',
-	description: 'Ping!',
-	execute(message) {
-        const timeTaken = Date.now() - message.createdTimestamp;
-		message.reply(`Pong! This message had a latency of ${timeTaken}ms.`);
-	},
+  name: 'ping',
+  description: 'Ping!',
+  execute(message) {
+    const timeTaken = Date.now() - message.createdTimestamp;
+    message.reply(`Pong! This message had a latency of ${timeTaken}ms.`);
+  },
 };
