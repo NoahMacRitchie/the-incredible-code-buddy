a2f07bc2075683414d9f2c099c1db48cdb559496
Hans Sy <55111820+hanssy130@users.noreply.github.com>
2021-03-14 08:22:27 -0700

    added helpful console log msg for missing anime api key

--- commands/anime.js
+++ commands/anime.js
@@ -19,7 +19,13 @@ module.exports = {
   help: 'Run !anime followed by the title of the anime you want to search up',
   execute(message, args) {
     // do not run if missing the API key
-    if (!process.env.ANIME_API_KEY) return;
+    if (!process.env.ANIME_API_KEY) {
+      console.log('Missing API key. Go to this link and add "x-rapidapi-key" into the .env');
+      console.log('You will need to signup on rapidapi to obtain a key.');
+      console.log('https://rapidapi.com/theapiguy/api/jikan1?endpoint=apiendpoint_e7aa0a5a-5b85-4785-a231-adab865f8d73');
+      console.log('Enter ANIME_API_KEY="your-rapidapi-key-here"');
+      return;
+    }
     if (!args[0]) {
       message.channel.send('To search for anime, type `!anime <search query>`.');
     } else {
