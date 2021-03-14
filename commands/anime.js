952797c1a0dce53ff6ea16b632c05bb1f8548384
Hans Sy <55111820+hanssy130@users.noreply.github.com>
2021-03-14 00:03:29 -0800

    YOU CAN LOOK UP ANIME TITLES NOW (#10)
    * YOU CAN LOOK UP ANIME TITLES NOW
    
    * Moved out api token into env variable
    
    Co-authored-by: agoryelov <andrey.goryelov@gmail.com>

---
+++ commands/anime.js
@@ -0,0 +1,35 @@
+const fetch = require('node-fetch');
+
+async function asyncTest(query) {
+  const response = await fetch(`https://jikan1.p.rapidapi.com/search/anime?q=${query}`, {
+    method: 'GET',
+    headers: {
+      'x-rapidapi-key': process.env.ANIME_API_KEY,
+      'x-rapidapi-host': 'jikan1.p.rapidapi.com',
+    },
+  });
+  const blob = await response.json();
+  const result = blob.results[0].url;
+  return result;
+}
+
+module.exports = {
+  name: 'anime',
+  description: 'Looks up anime.',
+  execute(message, args) {
+    if (!args[0]) {
+      message.channel.send('To search for anime, type `!anime <search query>`.');
+    } else {
+      let query = '';
+      for (let i = 0; i < args.length; i++) {
+        query = query.concat(args[i]);
+        query = query.concat('%20');
+      }
+      asyncTest(query).then((test) => {
+        message.channel.send(test);
+      });
+    }
+  },
+};
+
+// Made by Hans Sy
